f5f405e2323f3be2cc179a9dad16fadb530d5116
TaverasMisael <macrot29@gmail.com>
2017-02-06 13:30:42 -0400

    core: setup config and development eviroment

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const webpack = require("webpack");
+module.exports = {
+  context: path.resolve(__dirname, "./src"),
+  entry: {
+    app: "./scripts/main.js"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./dist/assets"),
+    publicPath: "/assets"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./src") // New
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        options: { presets: ["es2015"] },
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [new webpack.optimize.CommonsChunkPlugin({
+      name: "commons",
+      filename: "commons.js",
+      minChunks: 2
+    })]
+};
