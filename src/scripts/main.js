41d2596d551d34d945b72a0b54d15248abfc5e2d
TaverasMisael <macrot29@gmail.com>
2017-02-07 15:44:35 -0400

    feat: complete the main Slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,18 @@
 import '../scss/main.scss';
 import 'bootstrap/dist/js/bootstrap.min';
+import Siema from 'siema';
+
+activateMainSlider();
+
+function activateMainSlider() {
+  const mainSlider = new Siema({
+    selector: '.slider__container',
+    loop: true,
+
+  });
+
+  document.querySelector('.slider__control--backward')
+    .addEventListener('click', (e) => mainSlider.prev());
+  document.querySelector('.slider__control--forward')
+    .addEventListener('click', (e) => mainSlider.next());
+}
