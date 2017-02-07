c83e478b75b2ac47e1230d635d82b37e0610c989
TaverasMisael <macrot29@gmail.com>
2017-02-07 16:14:46 -0400

    feat: make the 'createSlider' a more generic Function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,17 +2,23 @@ import '../scss/main.scss';
 import 'bootstrap/dist/js/bootstrap.min';
 import Siema from 'siema';
 
-activateMainSlider();
+$(() => {
+  createSlider('.mainSlider');
+  createSlider('.projectSlider', {perPage: 3});
+})
 
-function activateMainSlider() {
-  const mainSlider = new Siema({
-    selector: '.slider__container',
-    loop: true,
+function createSlider(selector, options = {}) {
+  const config = Object.assign({}, {
+    selector,
+    loop: true
+  }, options);
+  const slider = new Siema(config);
 
-  });
 
-  document.querySelector('.slider__control--backward')
-    .addEventListener('click', (e) => mainSlider.prev());
-  document.querySelector('.slider__control--forward')
-    .addEventListener('click', (e) => mainSlider.next());
+  document.querySelector(`${selector}`).parentElement
+    .querySelector('.slider__control--backward')
+    .addEventListener('click', (e) => slider.prev());
+  document.querySelector(`${selector}`).parentElement
+    .querySelector('.slider__control--forward')
+    .addEventListener('click', (e) => slider.next());
 }
