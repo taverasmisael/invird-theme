1d6dc2b3c6ab1081380aabffcc10b33663dfb0fb
TaverasMisael <macrot29@gmail.com>
2017-02-28 12:31:50 -0400

    feat: allow multiple postsliders per page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,28 +3,31 @@ import 'bootstrap/dist/js/bootstrap.min';
 import Siema from 'siema';
 
 $(() => {
-  createSlider('.mainSlider');
-  createSlider('.projectSlider', { perPage: 3 });
+  createPostSlider('.postSlider');
 })
 
-function createSlider(selector, options = {}) {
-  const sliderElement = document.querySelector(`${selector}`);
-  if (sliderElement) {
-    const config = Object.assign({}, {
-      selector,
-      duration: 500,
-      easing: 'cubic-bezier(0.22, 0.61, 0.36, 1)',
-      loop: true
-    }, options);
-    const slider = new Siema(config);
+function createPostSlider(globalSelector) {
+  const slidersArray = Array.from(document.querySelectorAll(globalSelector));
+  if (slidersArray) {
+    slidersArray.forEach(el => {
+      const selector = `#${el.id}`;
+      const config = {
+        selector,
+        duration: 500,
+        easing: 'cubic-bezier(0.22, 0.61, 0.36, 1)',
+        loop: true,
+        perPage: 3
+      }
+      const slider = new Siema(config);
 
-    sliderElement.parentElement
-      .querySelector('.slider__control--backward')
-      .addEventListener('click', (e) => slider.prev());
-   sliderElement.parentElement
-      .querySelector('.slider__control--forward')
-      .addEventListener('click', (e) => slider.next());
-    } else {
-      return ;
-    }
+      el.parentElement
+        .querySelector('.slider__control--backward')
+        .addEventListener('click', (e) => slider.prev());
+      el.parentElement
+        .querySelector('.slider__control--forward')
+        .addEventListener('click', (e) => slider.next());
+    })
+  } else {
+    return;
+  }
 }
